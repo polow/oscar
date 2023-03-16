06ff2047f036363323b71c2b89eafd02cfb9a2d0
polow <polow2010@gmail.com>
2023-03-16 07:27:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,8 +14,13 @@ sudo apt install -y software-properties-common
 sudo apt install curl -y
 
 cd /tmp
-curl --output anaconda.sh https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
-sha256sum anaconda.sh -b
-bash anaconda.sh -y
+
+curl https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh --output anaconda.sh
+
+sha256sum anaconda.sh
+
+bash anaconda.sh -b -p $HOME/miniconda
+
 source ~/.bashrc
+
 conda list
