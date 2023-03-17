cdcd96d4752d1785520f69a6987eb2319958d55c
polow <polow2010@gmail.com>
2023-03-17 14:51:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,11 @@
 
 sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
 sudo apt-get -y update
-
-sudo apt-get install -y gdal
-
+sudo apt-get install -y gdal-bin
 ogrinfo --version
-
-
 sudo apt-get install -y libgdal-dev
-
-echo "Sssssss"
+echo "export"
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
-
 export C_INCLUDE_PATH=/usr/include/gdal
-
-pip install GDAL==3.0.4
-# pip install GDAL
+echo "end"
+pip install GDAL
