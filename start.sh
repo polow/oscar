36e2d879271c28a80a0a2a5a081fe680980df51d
polow <polow2010@gmail.com>
2023-03-17 12:54:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-sudo apt-get install -y software-properties-common
-sudo apt-get -y update
+
 sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
-# sudo apt-get -y update
-sudo apt-get install -y gdal-bin
+sudo apt-get -y update
 
-ogrinfo --version
-
-sudo apt install -y python3-numpy
+sudo apt-get install -y gdal
 
 ogrinfo --version
 
 
 sudo apt-get install -y libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+
 export C_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+
 pip install GDAL==3.4.3
