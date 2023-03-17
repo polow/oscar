b953ad052982926bc8b421e29624539bf4921d2b
polow <polow2010@gmail.com>
2023-03-18 00:01:24 +0200

    J

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ sudo apt-get install -y vlibgdal-dev
 sudo apt-get install -y libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
+gdal-config --version
+
 pip install GDAL
