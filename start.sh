1928e8aadd4e2f24b144c6747ecaf5b23c74cbc2
polow <polow2010@gmail.com>
2023-03-17 23:23:06 +0200

    341

--- start.sh
+++ start.sh
@@ -4,12 +4,7 @@ sudo apt -y update
 sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
 sudo apt-get -y update
 sudo apt-get install -y gdal-bin
-
-sudo apt-get install -y libgdal-dev
-sudo apt-get install -y binutils
-sudo apt-get install -y libproj-dev
-
+sudo apt-get install -yvlibgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-
-sudo apt-get install -y libheif-dev
+pip install GDAL==3.4.1
