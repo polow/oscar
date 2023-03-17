872ec16e8800dff822a4b67e4d96d9d71a4984f9
polow <polow2010@gmail.com>
2023-03-17 20:02:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,18 +4,18 @@ sudo apt -y update
 sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
 sudo apt-get -y update
 sudo apt-get install -y gdal-bin
-ogrinfo --version
+
 sudo apt-get install -y libgdal-dev
 sudo apt-get install -y binutils
 sudo apt-get install -y libproj-dev
-echo "export"
+
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-echo "end"
+
 sudo apt-get install -y libheif-dev
-sudo ldconfig
-pkg-config libheif --modversion
 pip install GDAL==3.4.1
+
 sudo ldconfig
-sudo ldconfig -v
+
 sudo find / -name libheif.so.1.so
+echo $LD_LIBRARY_PATH
