6694ac59c48507d9799e197e30fd44048516d5fb
polow <polow2010@gmail.com>
2023-03-17 23:39:58 +0200

    K

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y gdal-bin
 sudo apt-get install -yvlibgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL==3.4.1
+pip install GDAL
