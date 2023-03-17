78a2944c9b5546b80f6478a733726d4299668ee9
polow <polow2010@gmail.com>
2023-03-17 07:26:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
 sudo apt-get install -y gdal-bin
 
 ogrinfo --version
-pip install gcc==12.1.0
+
 sudo apt install -y python3-numpy
 
 ogrinfo --version
@@ -15,4 +15,4 @@ ogrinfo --version
 sudo apt-get install -y libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL
+pip install GDAL==3.4.3
