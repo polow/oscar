00a88edd36249248f2bcfdc14745756c60f5d389
polow <polow2010@gmail.com>
2023-03-18 19:29:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+
+
+
 sudo apt-get install -y software-properties-common
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y software-properties-common
-sudo apt-get install -y python3-dev
 
-sudo add-apt-repository -y ppa:ubuntugis/ppa && sudo apt-get -y update
-sudo apt-get -y update
-sudo apt-get install -y gdal-bin
+
+sudo add-apt-repository ppa:ubuntugis/ppa
+sudo apt-get update
+sudo apt-get install libgdal-dev gdal-bin
+
+# Sanity, check the version. For ubuntu 18.04, it's probably 2.4.2
 ogrinfo --version
-sudo apt-get install -y libgdal-dev
+
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-sudo echo "xx"
-pip install GDAL==3.4.3
-sudo echo "yy"
+
+sudo apt install python3.8-dev. <-- NOTE the python version should match yours
+pip3 install GDAL==$(ogrinfo --version | cut -d' ' -f2 | cut -d',' -f1)
