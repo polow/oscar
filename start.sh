12080f13ef8f12b9d95a24613816fd621baa2f60
polow <polow2010@gmail.com>
2023-03-19 00:34:25 +0200

    S

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+cd /opt
+git clone https://github.com/OSGeo/proj.4.git
+cd proj.4
+./configure
+make
+sudo checkinstall
+cd ..
+
+
 wget https://download.osgeo.org/gdal/3.4.3/gdal-3.4.3.tar.gz
 tar xzf gdal-3.4.3.tar.gz
 cd gdal-3.4.3
