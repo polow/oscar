8c8c968ccb5c190fb48017fb4e4f5a687403fa73
polow <polow2010@gmail.com>
2023-03-18 16:36:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt install -y software-properties-common
-
-sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get -y update
-sudo apt-get -y update
-sudo apt-get install -y gdal-bin
-sudo apt-get install -y vlibgdal-dev
-sudo apt-get install -y libgdal-dev
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-
-pip install GDAL==3.4.3
-
-sudo echo "XXXXXX"
-export LD_LIBRARY_PATH=/usr/local/lib
-
-ls /usr/local/lib
-
-sudo ldconfig
+sudo echo "Hello"
