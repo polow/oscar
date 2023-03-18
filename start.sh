8b82c4054dd5bf526927aa40d6531ea0a45ee6d8
polow <polow2010@gmail.com>
2023-03-18 16:45:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo echo "Hello"
+sudo apt-get install -y software-properties-common
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y software-properties-common
+
+sudo add-apt-repository -y ppa:ubuntugis/ppa && sudo apt-get -y update
+sudo apt-get -y update
+sudo apt-get install -y gdal-bin
+sudo apt-get install -y libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
