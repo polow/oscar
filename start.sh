a48a7383407c367b42eb9082a9c6cd057e4012c5
polow <polow2010@gmail.com>
2023-03-19 09:51:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,26 +5,6 @@ sudo apt-get install -y autoconf
 
 sudo apt-get install -y pkgconf
 
-cd /opt
-git clone https://github.com/OSGeo/proj.4.git
-cd proj.4
-autoreconf --install
-./configure
-make
-sudo checkinstall
-cd ..
-
 ls
-
-tar xzf gdal-3.4.3.tar.gz
-
-ls 
-cd gdal-3.4.3
-
 ls
-
-autoreconf --install
-./configure
-make # Go get some coffee, this takes a while.
-sudo make install
-cd ..
+ls
