7001898c10624b84f06f36da0eb4640b15247fbe
polow <polow2010@gmail.com>
2023-03-19 09:35:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y autoconf
 cd /opt
 git clone https://github.com/OSGeo/proj.4.git
 cd proj.4
-autoconf
+autoreconf --install
 ./configure
 make
 sudo checkinstall
@@ -17,7 +17,7 @@ wget https://download.osgeo.org/gdal/3.4.3/gdal-3.4.3.tar.gz
 tar xzf gdal-3.4.3.tar.gz
 cd gdal-3.4.3
 
-autoconf
+autoreconf --install
 ./configure
 make # Go get some coffee, this takes a while.
 sudo make install
