c1089808b37e9ee4aafaa810b33c477c50bb8acd
polow <polow2010@gmail.com>
2023-03-20 11:04:25 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,7 @@ sudo apt install -y python3-pip
 
 sudo apt-get -y update
 sudo apt-get install -y libgdal1i libgdal1-dev libgdal-dev
-sudo apt -y update
-sudo apt -y install -y gdal-bin
-pip install GDAL==3.4.1.10
+gdal-config --version
 
 
 
